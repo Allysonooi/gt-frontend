aa29b50e130bb5306b73640a12a7856edcbb16c3
Allyson Ooi <allysonooi28@gmail.com>
2022-01-22 16:04:56 +0800

    Setup actual routing on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from 'assets/logo.svg';
-import 'styles/App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MainLayout from 'components/layout/MainLayout';
+import routes from 'constants/routes';
 
 export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        {routes.map(({ path, component }) => (
+          <Route
+            exact
+            path={path}
+            render={() => <MainLayout>{component}</MainLayout>}
+          />
+        ))}
+      </Switch>
+    </Router>
   );
 };
