be1b3d15424f0fd1d23b2ad19b02d05dae439896
Allyson Ooi <allysonooi28@gmail.com>
2022-01-22 16:04:30 +0800

    Setup navigation - topbar and menu bar

---
+++ src/components/navigation/SideMenuBar.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Drawer from '@mui/material/Drawer';
+import Toolbar from '@mui/material/Toolbar';
+import SideMenuBarItem from './SideMenuBarItem';
+
+export default () => {
+  const drawerWidth = 240;
+
+  return (
+    <Drawer
+      variant="permanent"
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        [`& .MuiDrawer-paper`]: {
+          width: drawerWidth,
+          boxSizing: 'border-box',
+        },
+      }}
+    >
+      <Toolbar />
+      <SideMenuBarItem />
+    </Drawer>
+  );
+};
---
+++ src/components/navigation/SideMenuBarItem.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import routes from 'constants/routes';
+
+export default () => {
+  const history = useHistory();
+
+  return (
+    <Box sx={{ overflow: 'auto' }}>
+      <List>
+        {routes.map(({ label, path, icon, shownOnNavBar }) =>
+          shownOnNavBar ? (
+            <ListItemButton
+              onClick={() => {
+                history.push(path);
+              }}
+            >
+              <ListItemIcon>{icon}</ListItemIcon>
+              <ListItemText primary={label} />
+            </ListItemButton>
+          ) : (
+            <></>
+          ),
+        )}
+      </List>
+    </Box>
+  );
+};
