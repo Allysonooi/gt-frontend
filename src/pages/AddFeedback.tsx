4ef525d0eb828aa004bd0104569a86eb5885fafb
Allyson Ooi <allysonooi28@gmail.com>
2022-01-23 01:27:51 +0800

    Add caption for Add Feedback page, clean up

--- src/pages/AddFeedback.tsx
+++ src/pages/AddFeedback.tsx
@@ -7,6 +7,9 @@ export default () => {
       <Typography variant="h5" className="pb20">
         Add New Feedback
       </Typography>
+      <Typography variant="subtitle1">
+        Please input your details below and submit.
+      </Typography>
       <NewSubmissionFormFields />
     </>
   );
